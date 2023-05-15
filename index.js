755b163f0d22ec1dc766029aa24f03aa2b0bf1b5
Dan McAtee <danielmcatee@me.com>
2023-05-15 09:14:49 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Print out welcome message
+
+// Store player name
+// Welcome player
+
+// Create a file called questions.json
+// Get the quedtions from the json file
+
+// Keep track if the game loop should continue running
+
+// Write a function called play that will run the game loop
+//  Kepp track of the score
+// Start the game loop
+// Ask a question and store the answer
+// Check if the answer is correct
+// If the answer is correct, print out "Awesome your answer is correct" and increment the score by 2
+// If the answer is wrong, print out "Sorry your answer is wrong" and decrement the score by 1
+// Print out the score
+
+// Print out final score after every question was answered
+// Add users score to the high score list in a file called scores.json
+// Get all the scores
+// Print out high score table
+// Check if highscore was beaten
+
+// Ask user if they want to play again
+// If no stop the game loop
